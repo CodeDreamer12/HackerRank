2f0fc7fe39cd0f0e40eec7dd5991eb791c838b76
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-09-07 01:25:27 +0530

    Create Basic Data Types.cpp

---
+++ Prepare/In C++/Basic Data Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+ int a;
+ long b;
+ char ch;
+ float d;
+ double e;
+ cin>>a>>b>>ch>>d>>e;
+ cout<<a<<endl;
+ cout<<b<<endl;
+ cout<<ch<<endl;
+ cout.precision(3);
+ cout<<fixed<<d<<endl;
+ cout.precision(9);
+ cout<<fixed<<e<<endl;
+    return 0;
+}
